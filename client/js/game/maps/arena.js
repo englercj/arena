44f32dde1986801de1d8d507e895a9350db6e4d1
Chad Engler <englercj@live.com>
2012-09-21 13:03:20 -0400

    Modified map format a bit

--- client/js/game/maps/arena.js
+++ client/js/game/maps/arena.js
@@ -1,5 +1,5 @@
 define({
-    terrain: {}, //three.js v3.1 Model Mesh (JSON)
-    spawns: [{ x: 0, y: 0, z: 0 }], //array of spawn points in the map
-    entities: [{ x: 0, y: 0, z: 0, t: 0 }] //array of entities to place in the world
+    entities: [{ x: 0, y: 0, z: 0, t: 0 }], //array of static entities to place in the world; { coords (x,y,z) and type (t) }
+    spawns: [{ x: 0, y: 0, z: 0, t: 0 }], //array of spawn points in the map { coords (x,y,z) and type (t) }
+    level: {} //three.js v3.1 Model Mesh (JSON)
 });
