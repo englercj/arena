221043aaab3015df1b3aee770c4e735f6276ef6a
Chad Engler <englercj@live.com>
2012-09-24 09:35:25 -0400

    Changed texturePath to be relative

--- client/js/game/resources.js
+++ client/js/game/resources.js
@@ -1,7 +1,7 @@
 define({
     maps: [
-        { name: 'subway', file: 'js/game/maps/subway/subway.json', texturePath: '/js/game/maps/subway' },
-        { name: 'arena', file: 'js/game/maps/arena/arena.json', texturePath: '/js/game/maps/arena' }
+        { name: 'subway', file: 'js/game/maps/subway/subway.json', texturePath: 'js/game/maps/subway' },
+        { name: 'arena', file: 'js/game/maps/arena/arena.json', texturePath: 'js/game/maps/arena' }
     ],
     models: [
         
